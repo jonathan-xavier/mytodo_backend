b628cca074e8d35343ef2da5cd0f596e6e48934b
Jonathan <jonathanxavier556@gmail.com>
2020-11-04 19:02:35 -0300

    Finalizando o update do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,8 +21,36 @@ class UserController{
 
     //fazer alterações dos dados cadastrais
     async update(req,res){
-            console.log(req.userId);
-        return res.json({ok: true});
+            
+        const {email, oldPassword} = req.body;
+        const user = await User.findByPk(req.userId);
+
+            //essa verificação é caso ele esteja mudando de email
+            //se o email que ele esta passando é diferente do que ele ja tem
+            //e se não tem nenhum usuário com esse email novo que ele está passsando.
+        if(email !== user.email){
+            const userExists = await User.findOne({
+                where: {email: req.body.email}
+            });
+            if(userExists){
+                return res.status(400).json({error: 'Usuário já existe.'});
+    
+            }
+        }
+
+        //verifica se a senha antiga bate com a que ele ja tem.
+        //apenas se ele passar a senha antiga.
+        if(oldPassword && !(await user.checkPassword(oldPassword))){
+            return res.status(401).json({error: 'Senha incorreta!'});
+        } 
+                                    //metodo que atualiza no banco
+        const {id, nome} = await user.update(req.body);
+
+        return res.json({
+            id,
+            nome,
+            email,
+        });
     }
 }
 
